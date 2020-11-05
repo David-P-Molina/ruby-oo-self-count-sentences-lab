0c713e3a21ccd0fe7cb13f7b3b4eac6de4f233ce
David Moina <davemolina777@gmail.com>
2020-11-04 22:59:29 -0600

    commit tree 17cd334873e90773025891f40a5fad298d57f57a

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,36 @@ require 'pry'
 class String
 
   def sentence?
+    if self.include? (".")
+      true
+    else
+      false
+    end
     
   end
 
   def question?
-
+    if self.include?("?")
+      true
+    else
+      false
+    end
   end
 
   def exclamation?
-
+    if self.include?("!")
+      true
+    else 
+      false
+    end
   end
 
   def count_sentences
-
+   # binding.pry
+    if self.split.length > 0 
+      self.split(/[?!.]\s/).length
+    elsif self.split.length == 0
+      p 0
+    end
   end
 end
